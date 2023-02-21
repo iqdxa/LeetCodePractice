d23060306f683815bc3059fa78a13f2ccc87ac62
iqdxa <1429316040@qq.com>
2023-02-21 22:59:37 +0800

    9

---
+++ 9.回文数.c
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=c
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+bool isPalindrome(int x){
+    int num[20]={0};
+    int count=0;
+    if(x<0)
+    {
+        return false;
+    }else if(x==0)
+    {
+        return true;
+    }else
+    {
+        for(int i=0;i<20;i++)
+        {
+            num[i]=x%10;
+            x/=10;
+            count++;
+            if(x==0)
+            {
+                break;
+            }
+        }
+    }
+    for(int i=0;i<count/2;i++)
+    {
+        if(num[i]!=num[count-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+// @lc code=end
+
